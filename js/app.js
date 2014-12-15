5d62733931511d5fc41be447a3404606a367bb9d
Adam Gralinski <adam.gralinski@roche.com>
2014-12-15 15:21:32 +0100

    draw matrix function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,52 @@
 $(document).ready(function(){
     $('.about_me').on('click', function(){
         $("#about_me--dialog").dialog();
-        
     });
-    
 });
 
 
 var LUFactor = angular.module('LUFactor', []);
 
-LUFactor.controller('lufStartCtrl', ['$scope', function($scope){
-    $scope.greeting=""
-    
+LUFactor.service('matrixResources',function(){
+        matrix ={
+            size : 3,
+            array : []
+        };
+        return matrix;
+});
+
+LUFactor.controller('lufCtrl', ['$scope', function($scope){
+
 }]);
+
+LUFactor.controller('matrixSizeCtrl', ['$scope', 'matrixResources', function($scope, matrixResources){
+        $scope.matrix = matrixResources;
+
+        $scope.update = function(sizeForm) {
+            matrixResources.size = angular.copy(sizeForm.size);
+        };
+        $scope.reset = function() {
+            $scope.matrix = {};
+        };
+        $scope.reset();
+}]);
+
+LUFactor.controller('matrixValCtrl', ['$scope', 'matrixResources', function($scope, matrixResources){
+    $scope.matrix = matrixResources;
+    $scope.len = function(){
+        array =[];
+        for (i=0; i<matrix.size;i++){
+            array.push(i);
+        }
+        return array;
+    };
+    $scope.refresh = function(){
+      $scope.$apply();
+    };
+    $scope.draw = function(){
+
+    };
+}]);
+
+
+

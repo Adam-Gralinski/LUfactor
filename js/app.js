44f46f0ebca87f70a7f0a979de6a9582af8615d1
Adam-Gralinski <agralinski@gmail.com>
2014-12-22 21:55:01 +0100

    prototype of LU algorithm

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ LUFactor.service('matrixResources', function () {
             }
         },
         draw: function(){
-            return numeric.LU(this.array);
+            //return numeric.cLU(this.array);
         },
         clearArray: function(){
            this.initMatrix();
